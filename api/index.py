ad9f6450ea994f033c6604e2a4fa8fbe30d2f741
DollarEat <115690374+DollarEat@users.noreply.github.com>
2022-12-15 15:34:37 +0800

    index.py

--- api/index.py
+++ api/index.py
@@ -1,21 +1,11 @@
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from api.chatgpt import ChatGPT
-
-import os
-
-line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
-line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
-working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
+from flask import Flask
 
 app = Flask(__name__)
-chatgpt = ChatGPT()
 
-# domain root
 @app.route('/')
 def home():
     return 'Hello, World!'
 
-
+@app.route('/about')
+def about():
+    return 'About'
